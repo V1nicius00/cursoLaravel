0177c5ee15b71444bd6247b9f4546891a4592989
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-03-24 16:02:52 -0300

    feat: creating page products

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.layout')
+@section('title', 'Home')
+
+@section("content")
+    
+<div class="row container">
+    @foreach ($products as $product)
+    <div class="col s12 m4">
+        <div class="card">
+            <div class="card-image">
+              <img src="{{ $product->image }}">
+              <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">visibility</i></a>
+            </div>
+            <div class="card-content">
+                <span class="card-title">{{ Str::limit($product->name, 10) }}</span>
+              <p>{{ Str::limit($product->description, 25) }}</p>
+            </div>
+          </div>
+    </div>
+    @endforeach
+</div>
+
+@endsection
+
+            
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/products', function () {
+
+    $products = Product::all();
+    return view('home', compact('products'));
 });
