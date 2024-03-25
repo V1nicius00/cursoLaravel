3f478d16469d5e5dd3bd6d71e8450d9c50c3f553
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-03-24 22:45:40 -0300

    feat: adding categories menu on layout

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.layout')
-@section('title', '{{Str::limit($product->name, 5)}}}')
+@section('title', Str::limit($product->name, 5))
 
 @section("content")
 
@@ -10,6 +10,8 @@
     <div class="col s12 m6">
         <h1>{{ Str::limit($product->name, 12) }}</h1>
         <p>{{ $product->description }}</p>
+        <p>Posted by: {{ $product->user->firstName }}</p>
+        <p>Category: {{ $product->category->name }}</p>
         <button class="btn orange btn-large">Buy Now</button>
     </div>
 </div>
